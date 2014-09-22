92e9128cb0ccdc18deb5a8fe57db37c245e8c92b
James Glover <james.glover@sanger.ac.uk>
2014-09-22 16:32:40 +0100

    Fix described_class nesting changes in rspec
    described_class now returns the nested class, not
    the outer class. So we need to adjust our specs.

--- spec/support/it_behaves_like_maps_json_fields.rb
+++ spec/support/it_behaves_like_maps_json_fields.rb
@@ -1,12 +1,12 @@
 shared_examples_for 'maps JSON fields' do |mapped_attributes|
   let!(:json_handler) { described_class.send(:json) }
 
-  describe described_class.send(:json) do
+  context "Handler" do
     subject { json_handler.new(json) }
 
     mapped_attributes.each do |from, to|
       it "maps #{from.inspect} to #{to.inspect}" do
-        expect(subject[to]).to eq(json[from])
+        expect(subject[to]).to eq(subject[from])
       end
     end
   end
@@ -15,7 +15,7 @@ end
 shared_examples_for 'ignores JSON fields' do |ignored_attributes|
   let!(:json_handler) { described_class.send(:json) }
 
-  describe described_class.send(:json) do
+  context "Handler" do
     ignored_attributes.each do |name|
       it "ignores #{name.inspect}" do
         expect(json_handler.new(json.merge(name => 'ignored'))).to_not have_key(name)
