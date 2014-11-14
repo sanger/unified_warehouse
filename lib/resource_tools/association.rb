d3bd06bcbd8c628ea4bb2935312365f3c853063c
James Glover <james.glover@sanger.ac.uk>
2014-11-14 10:47:09 +0000

    Missing record exceptions are more informative

--- lib/resource_tools/association.rb
+++ lib/resource_tools/association.rb
@@ -13,9 +13,14 @@ module ResourceTools::Association
         attr_accessor :#{association}_id, :#{association}_uuid
 
         before_validation do
-          associate = #{association.to_s.classify}.with_uuid(#{association}_uuid).first! unless #{association}_uuid.blank?
-          associate ||= #{association.to_s.classify}.for_lims(id_lims).with_id(#{association}_id).first! unless #{association}_id.blank?
-          self.#{association}= associate unless associate.blank?
+          uuid_error = "No #{association} with uuid '%s'"
+          id_error   = "No #{association} for '%s' with_id '%s'"
+
+          associate = #{association.to_s.classify}.with_uuid(#{association}_uuid).first or
+            raise(ActiveRecord::RecordNotFound, uuid_error % #{association}_uuid) unless #{association}_uuid.blank?
+          associate ||= #{association.to_s.classify}.for_lims(id_lims).with_id(#{association}_id).first or
+            raise(ActiveRecord::RecordNotFound, id_error % [id_lims,#{association}_id]) unless #{association}_id.blank?
+          self.#{association}= associate unless associate.nil?
         end
 
       }
