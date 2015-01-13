cd44d46852e9e335b12adc8a3a817ad15e1fb603
James Glover <james.glover@sanger.ac.uk>
2015-01-13 12:09:45 +0000

    Fix missing method around migration
    No idea how this one happened.

--- db/migrate/20141120123833_increase_tag_set_name_column_width.rb
+++ db/migrate/20141120123833_increase_tag_set_name_column_width.rb
@@ -1,6 +1,9 @@
 class IncreaseTagSetNameColumnWidth < ActiveRecord::Migration
+
+  def up
     change_table :iseq_flowcell do |t|
       t.change "tag_set_name",     :string,  limit: 100, comment:'WTSI-wide tag set name'
+    end
   end
 
   def down
