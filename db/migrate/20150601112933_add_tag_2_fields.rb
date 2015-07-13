df63ed62432f375431395543f1046a14a285ec60
James Glover <james.glover@sanger.ac.uk>
2015-07-13 10:08:03 +0100

    Do not record tag2 index
    Downstream processes require a single identifier to reference an aliquot
    within a pool. Further constraints require that this be the tag_index field.
    Given that the behaviour of this field has now changed, and now merely reflects
    the index of an aliquot within the pool, tag2 index would be even more
    misleading.

--- db/migrate/20150601112933_add_tag_2_fields.rb
+++ db/migrate/20150601112933_add_tag_2_fields.rb
@@ -1,10 +1,9 @@
 class AddTag2Fields < ActiveRecord::Migration
   def change
     change_table :iseq_flowcell do |t|
-      t.integer  'tag_2_index',                   comment: 'Tag 2 index, NULL if there is no second tag'
-      t.string   'tag_2_sequence',    limit: 30,  comment: 'Tag sequence for tag 2'
-      t.string   'tag_2_set_id_lims', limit: 20,  comment: 'LIMs-specific identifier of the tag set for tag 2'
-      t.string   'tag_2_set_name',    limit: 100, comment: 'WTSI-wide tag set name for tag 2'
+      t.string   'tag2_sequence',    limit: 30,  comment: 'Tag sequence for tag 2'
+      t.string   'tag2_set_id_lims', limit: 20,  comment: 'LIMs-specific identifier of the tag set for tag 2'
+      t.string   'tag2_set_name',    limit: 100, comment: 'WTSI-wide tag set name for tag 2'
     end
   end
 end
