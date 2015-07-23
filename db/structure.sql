4120516feb01e6ffe168694358a11c9be133b80a
James Glover <james.glover@sanger.ac.uk>
2015-07-23 12:19:17 +0100

    Use after to set column locations

--- db/structure.sql
+++ db/structure.sql
@@ -76,6 +76,9 @@ CREATE TABLE `iseq_flowcell` (
   `tag_sequence` varchar(30) DEFAULT NULL COMMENT 'Tag sequence',
   `tag_set_id_lims` varchar(20) DEFAULT NULL COMMENT 'LIMs-specific identifier of the tag set',
   `tag_set_name` varchar(100) DEFAULT NULL COMMENT 'WTSI-wide tag set name',
+  `tag2_sequence` varchar(30) DEFAULT NULL COMMENT 'Tag sequence for tag 2',
+  `tag2_set_id_lims` varchar(20) DEFAULT NULL COMMENT 'LIMs-specific identifier of the tag set for tag 2',
+  `tag2_set_name` varchar(100) DEFAULT NULL COMMENT 'WTSI-wide tag set name for tag 2',
   `is_spiked` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'Boolean flag indicating presence of a spike',
   `pipeline_id_lims` varchar(60) DEFAULT NULL COMMENT 'LIMs-specific pipeline identifier that unambiguously defines library type',
   `bait_name` varchar(50) DEFAULT NULL COMMENT 'WTSI-wide name that uniquely identifies a bait set',
@@ -87,9 +90,6 @@ CREATE TABLE `iseq_flowcell` (
   `legacy_library_id` int(11) DEFAULT NULL COMMENT 'Legacy library_id for backwards compatibility.',
   `id_library_lims` varchar(255) DEFAULT NULL COMMENT 'Earliest LIMs identifier associated with library creation',
   `team` varchar(255) DEFAULT NULL COMMENT 'The team responsible for creating the flowcell',
-  `tag2_sequence` varchar(30) DEFAULT NULL COMMENT 'Tag sequence for tag 2',
-  `tag2_set_id_lims` varchar(20) DEFAULT NULL COMMENT 'LIMs-specific identifier of the tag set for tag 2',
-  `tag2_set_name` varchar(100) DEFAULT NULL COMMENT 'WTSI-wide tag set name for tag 2',
   PRIMARY KEY (`id_iseq_flowcell_tmp`),
   KEY `iseq_flowcell_id_lims_id_flowcell_lims_index` (`id_lims`,`id_flowcell_lims`),
   KEY `iseq_flowcell_sample_fk` (`id_sample_tmp`),
@@ -240,7 +240,7 @@ CREATE TABLE `study_users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-06-01 12:39:28
+-- Dump completed on 2015-07-23 12:18:20
 INSERT INTO schema_migrations (version) VALUES ('20141113110635');
 
 INSERT INTO schema_migrations (version) VALUES ('20141113130813');
