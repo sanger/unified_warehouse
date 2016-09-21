c8109186f0e4ae204bcb0d5e4341774df8f60674
James Glover <james.glover@sanger.ac.uk>
2016-09-21 16:37:35 +0100

    SS520 - Match Sequencescape idosyncracies

--- spec/models/stock_resource_spec.rb
+++ spec/models/stock_resource_spec.rb
@@ -26,7 +26,7 @@ describe StockResource do
 
   context 'for a well' do
     # We have a row for the lane, the sample and the control
-    let(:expected_entries) { 2 }
+    let(:expected_entries) { 1 }
 
     it_behaves_like 'a stock material'
 
@@ -39,6 +39,44 @@ describe StockResource do
         "study_uuid" => "000000-0000-0000-0000-0000000001",
 
         "stock_resource_id" => "12345",
+        "stock_resource_uuid" => "000000-0000-0000-0000-0000000002",
+
+        # We don't validate the barcode format, other
+        # LIMS might use different standards
+        "labware_machine_barcode" => "1220456987123",
+        "labware_human_barcode" => "DN456987D",
+        "labware_coordinate" => "A1",
+        "labware_type" => "well",
+
+        "current_volume" => 1.23,
+        "initial_volume" => 4.56,
+        "concentration"  => 23.56,
+
+        "gel_pass" => "Pass",
+        "pico_pass" => "Pass",
+        "snp_count" => 2,
+        "measured_gender" => "Unknown"
+
+      }
+    end
+
+  end
+
+  context 'for a well with SS formatted data' do
+    # We have a row for the lane, the sample and the control
+    let(:expected_entries) { 1 }
+
+    it_behaves_like 'a stock material'
+
+    let(:json) do
+      {
+        "created_at" => "2012-03-11T10:22:42+00:00",
+        "updated_at" => "2012-03-11T10:22:42+00:00",
+
+        "sample_uuid" => "000000-0000-0000-0000-0000000000",
+        "study_uuid" => "000000-0000-0000-0000-0000000001",
+
+        "stock_resource_id" => 12345,
         "stock_resource_uuid" => "000000-0000-0000-0000-0000000002",
 
         # We don't validate the barcode format, other
