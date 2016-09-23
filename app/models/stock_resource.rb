2e58a043b11a932ec80774fb854ed1ae1453f849
James Glover <james.glover@sanger.ac.uk>
2016-09-23 15:49:45 +0100

    Add test for update

--- app/models/stock_resource.rb
+++ app/models/stock_resource.rb
@@ -1,9 +1,11 @@
 class StockResource < ActiveRecord::Base
   include ResourceTools
-  include SingularResourceTools
+  include CompositeResourceTools
 
   has_associated(:study)
   has_associated(:sample)
+
+  has_composition_keys(:id_stock_resource_lims,:id_sample_tmp)
 
   json do
 
@@ -11,6 +13,11 @@ class StockResource < ActiveRecord::Base
       :stock_resource_id => :id_stock_resource_lims,
       :uuid              => :stock_resource_uuid
     )
+
+    has_nested_model(:samples) do
+    end
+
+    ignore(:samples)
 
   end
 
--- spec/models/stock_resource_spec.rb
+++ spec/models/stock_resource_spec.rb
@@ -9,7 +9,7 @@ describe StockResource do
     mock_sample; mock_study
   end
 
-  shared_examples_for 'a stock material' do
+  shared_examples_for 'a stock resource' do
 
     it_behaves_like 'maps JSON fields', {
     }
@@ -17,26 +17,29 @@ describe StockResource do
     it_behaves_like 'ignores JSON fields', [
     ]
 
-    it_behaves_like 'belongs to', [
-      [:study,:sample]
-    ]
+    it_behaves_like 'belongs to',[
+     :sample,
+     :study
+    ], [:samples]
 
-    it_behaves_like 'a singular resource'
+    it_behaves_like 'a nested resource'
   end
 
   context 'for a well' do
     # We have a row for the lane, the sample and the control
     let(:expected_entries) { 1 }
 
-    it_behaves_like 'a stock material'
+    it_behaves_like 'a stock resource'
 
     let(:json) do
       {
         "created_at" => "2012-03-11 10:22:42",
         "updated_at" => "2012-03-11 10:22:42",
 
-        "sample_uuid" => "000000-0000-0000-0000-0000000000",
-        "study_uuid" => "000000-0000-0000-0000-0000000001",
+        "samples" => [
+          "sample_uuid" => "000000-0000-0000-0000-0000000000",
+          "study_uuid" => "000000-0000-0000-0000-0000000001"
+        ],
 
         "stock_resource_id" => "12345",
         "stock_resource_uuid" => "000000-0000-0000-0000-0000000002",
@@ -60,21 +63,43 @@ describe StockResource do
       }
     end
 
+    context 'when update with identical tag indexes' do
+
+      let(:example_lims) { 'example' }
+
+      let(:updated_json) do
+        updated_json = json.dup
+        updated_json['current_volume'] = 1.00
+        updated_json['updated_at'] = "2012-03-11 10:22:42"
+        updated_json
+      end
+
+      it 'reuses the existing records' do
+        described_class.create_or_update_from_json(json, example_lims)
+        original_ids = described_class.pluck(:id_stock_resource_tmp,:id_sample_tmp)
+        described_class.create_or_update_from_json(updated_json, example_lims)
+        new_ids = described_class.pluck(:id_stock_resource_tmp,:id_sample_tmp)
+        expect(new_ids).to eq(original_ids)
+      end
+    end
+
   end
 
   context 'for a well with SS formatted data' do
     # We have a row for the lane, the sample and the control
     let(:expected_entries) { 1 }
 
-    it_behaves_like 'a stock material'
+    it_behaves_like 'a stock resource'
 
     let(:json) do
       {
         "created_at" => "2012-03-11T10:22:42+00:00",
         "updated_at" => "2012-03-11T10:22:42+00:00",
 
-        "sample_uuid" => "000000-0000-0000-0000-0000000000",
-        "study_uuid" => "000000-0000-0000-0000-0000000001",
+        "samples" => [
+          "sample_uuid" => "000000-0000-0000-0000-0000000000",
+          "study_uuid" => "000000-0000-0000-0000-0000000001"
+        ],
 
         "stock_resource_id" => 12345,
         "stock_resource_uuid" => "000000-0000-0000-0000-0000000002",
@@ -110,8 +135,10 @@ describe StockResource do
         "created_at" => "2012-03-11 10:22:42",
         "updated_at" => "2012-03-11 10:22:42",
 
-        "sample_uuid" => "000000-0000-0000-0000-0000000000",
-        "study_uuid" => "000000-0000-0000-0000-0000000001",
+        "samples" => [
+          "sample_uuid" => "000000-0000-0000-0000-0000000000",
+          "study_uuid" => "000000-0000-0000-0000-0000000001"
+        ],
 
         "stock_resource_id" => "12345",
         "stock_resource_uuid" => "000000-0000-0000-0000-0000000002",
