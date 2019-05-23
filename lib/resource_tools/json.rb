ebb6c463ac647bed5040013576ca3a5a79bbb152
James Glover <JamesGlover@users.noreply.github.com>
2019-05-23 10:08:52 +0100

    Remove unused block argument

--- lib/resource_tools/json.rb
+++ lib/resource_tools/json.rb
@@ -3,7 +3,7 @@ module ResourceTools::Json
 
   module ClassMethods
     def create_or_update_from_json(json_data, lims)
-      ActiveRecord::Base.transaction do |t|
+      ActiveRecord::Base.transaction do
         create_or_update(json.collection_from(json_data, lims))
       end
     end
