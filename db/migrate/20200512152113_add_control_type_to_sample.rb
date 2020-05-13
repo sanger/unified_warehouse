16cbab90d02cfb126ac6b18b5656768ee8cca5ad
Katy Taylor <kt17@sanger.ac.uk>
2020-05-13 10:24:32 +0100

    Make rubocop happy

--- db/migrate/20200512152113_add_control_type_to_sample.rb
+++ db/migrate/20200512152113_add_control_type_to_sample.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
+# Add control_type column, to store whether a control is positive or negative
+# Added for the Heron project
+# Works in conjunction with control column
 class AddControlTypeToSample < ActiveRecord::Migration
   def change
     add_column :sample, :control_type, :string
