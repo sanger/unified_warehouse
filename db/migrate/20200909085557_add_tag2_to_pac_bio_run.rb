857af779543ae1d843edb41319f540ee35f44c60
Stephen Inglis <si5@sanger.ac.uk>
2020-09-09 10:16:49 +0100

    rubocopped.

--- db/migrate/20200909085557_add_tag2_to_pac_bio_run.rb
+++ db/migrate/20200909085557_add_tag2_to_pac_bio_run.rb
@@ -1,10 +1,13 @@
+# frozen_string_literal: true
+
+# Add tag2 information to the pac_bio_run table so we can do dual indexing
 class AddTag2ToPacBioRun < ActiveRecord::Migration
   def change
-    change_table :pac_bio_run do |t|
+    change_table :pac_bio_run, bulk: true do |t|
       t.string   'tag2_sequence',    limit: 30,  comment: 'Tag sequence for tag 2', after: :tag_set_name
       t.string   'tag2_set_id_lims', limit: 20,  comment: 'LIMs-specific identifier of the tag set for tag 2', after: :tag2_sequence
       t.string   'tag2_set_name',    limit: 100, comment: 'WTSI-wide tag set name for tag 2', after: :tag2_set_id_lims
-      t.string   'tag2_identifier', limit: 30,  comment: 'The position of tag2 within the tag group', after: :tag2_set_name
+      t.string   'tag2_identifier', limit: 30, comment: 'The position of tag2 within the tag group', after: :tag2_set_name
     end
   end
 end
