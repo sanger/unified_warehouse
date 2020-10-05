6080c6b3dcc997357916ff945fe7daa89071af78
Andrew Sparkes <as28@sanger.ac.uk>
2020-10-05 10:18:12 +0100

    rubocop

--- db/migrate/20200929142921_add_ct_columns_to_lighthouse_samples.rb
+++ db/migrate/20200929142921_add_ct_columns_to_lighthouse_samples.rb
@@ -6,16 +6,16 @@ class AddCtColumnsToLighthouseSamples < ActiveRecord::Migration
     change_table :lighthouse_sample, bulk: true do |t|
       t.string  :ch1_target, null: true, comment: 'Target for channel 1', after: :lab_id
       t.string  :ch1_result, null: true, comment: 'Result for channel 1', after: :ch1_target
-      t.decimal :ch1_cq, precision: 11, scale: 8, null: true, comment: 'Cq value for channel 1',  after: :ch1_result
+      t.decimal :ch1_cq, precision: 11, scale: 8, null: true, comment: 'Cq value for channel 1', after: :ch1_result
       t.string  :ch2_target, null: true, comment: 'Target for channel 2', after: :ch1_cq
       t.string  :ch2_result, null: true, comment: 'Result for channel 2', after: :ch2_target
-      t.decimal :ch2_cq, precision: 11, scale: 8, null: true, comment: 'Cq value for channel 2',  after: :ch2_result
+      t.decimal :ch2_cq, precision: 11, scale: 8, null: true, comment: 'Cq value for channel 2', after: :ch2_result
       t.string  :ch3_target, null: true, comment: 'Target for channel 3', after: :ch2_cq
       t.string  :ch3_result, null: true, comment: 'Result for channel 3', after: :ch3_target
-      t.decimal :ch3_cq, precision: 11, scale: 8, null: true, comment: 'Cq value for channel 3',  after: :ch3_result
+      t.decimal :ch3_cq, precision: 11, scale: 8, null: true, comment: 'Cq value for channel 3', after: :ch3_result
       t.string  :ch4_target, null: true, comment: 'Target for channel 4', after: :ch3_cq
       t.string  :ch4_result, null: true, comment: 'Result for channel 4', after: :ch4_target
-      t.decimal :ch4_cq, precision: 11, scale: 8, null: true, comment: 'Cq value for channel 4',  after: :ch4_result
+      t.decimal :ch4_cq, precision: 11, scale: 8, null: true, comment: 'Cq value for channel 4', after: :ch4_result
     end
   end
 end
