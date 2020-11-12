96d58a000f8c54cbbd8fc96ea0e8eb00aa21ef5d
James Glover <james.glover@sanger.ac.uk>
2020-11-12 10:10:23 +0000

    Add indexes to sanger_sample_id and supplier_name columns
    As requested by David H:
    >We’re helping Keiran R to improve the performance of a couple of queries which he is running on the Multi-LIMS warehouse.  Experimenting with a clone of the mlwarehoue database, we’ve found that adding two indexes to the ‘sample’ table would help to turn a query that currently takes 90 seconds to one which will run in 0.1 second.
    
    >Please could you add indexes on the ‘supplier_name’ and ‘sanger_sample_id’ columns.  This barely increases the total size of the indexes on this table, so it will have no adverse impact on other users, and may even help anyone who is running queries similar to Keiran’s.  Adding the indexes can be done at any time, since MySQL does not lock a table when a secondary index is created.  In any case, each index takes less than a minute to add.
    
    Fixes #139

--- db/structure.sql
+++ db/structure.sql
@@ -314,7 +314,9 @@ CREATE TABLE `sample` (
   UNIQUE KEY `index_sample_on_id_sample_lims_and_id_lims` (`id_sample_lims`,`id_lims`),
   UNIQUE KEY `sample_uuid_sample_lims_index` (`uuid_sample_lims`),
   KEY `sample_accession_number_index` (`accession_number`),
-  KEY `sample_name_index` (`name`)
+  KEY `sample_name_index` (`name`),
+  KEY `index_sample_on_supplier_name` (`supplier_name`),
+  KEY `index_sample_on_sanger_sample_id` (`sanger_sample_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=49650 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 DROP TABLE IF EXISTS `samples_extraction_activity`;
@@ -514,6 +516,7 @@ INSERT INTO `schema_migrations` (version) VALUES
 ('20200929142921'),
 ('20201028091922'),
 ('20201029150039'),
-('20201103161806');
+('20201103161806'),
+('20201112100120');
 
 
