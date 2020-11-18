08aefd12007766ebeaf9076d1b49963bb3fe9f15
Andrew Sparkes <as28@sanger.ac.uk>
2020-11-18 15:01:11 +0000

    Added columns for uuids into lighthouse sample table Added missing and new columns to factory

---
+++ db/migrate/20201118142202_add_uuid_columns_to_lighthouse_samples.rb
@@ -0,0 +1,8 @@
+class AddUuidColumnsToLighthouseSamples < ActiveRecord::Migration[6.0]
+  def change
+    change_table :lighthouse_sample, bulk: true do |t|
+      t.string :lh_sample_uuid, limit: 36, null: true, comment: 'Sample uuid created in crawler', after: :filtered_positive_timestamp
+      t.string :lh_source_plate_uuid, limit: 36, null: true, comment: 'Source plate uuid created in crawler', after: :lh_sample_uuid
+    end
+  end
+end
