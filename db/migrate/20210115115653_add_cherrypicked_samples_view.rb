18603a4c9f94646f3904dc1ba12f89e3d201f4c5
Christopher Friend <christopher.friend@tessella.com>
2021-01-18 11:10:17 +0000

    Minor view changes

--- db/migrate/20210115115653_add_cherrypicked_samples_view.rb
+++ db/migrate/20210115115653_add_cherrypicked_samples_view.rb
@@ -6,9 +6,10 @@ class AddCherrypickedSamplesView < ActiveRecord::Migration[6.0]
     event_wh_db = Rails.application.config.event_wh_db
     execute <<-SQL.squish
       CREATE VIEW cherrypicked_samples AS
-      SELECT description, plate_barcode, phenotype, coordinate, created, event_type FROM (
-        SELECT mlwh_sample.description, mlwh_stock_resource.labware_human_barcode AS "plate_barcode", mlwh_sample.phenotype,
-        mlwh_stock_resource.labware_coordinate AS "coordinate", mlwh_sample.created, mlwh_events_event_types.key AS "event_type"
+      SELECT root_sample_id, plate_barcode, phenotype, coordinate, created, event_type
+      FROM (
+        SELECT mlwh_sample.description AS "root_sample_id", mlwh_stock_resource.labware_human_barcode AS "plate_barcode", mlwh_sample.phenotype AS "phenotype",
+        mlwh_stock_resource.labware_coordinate AS "coordinate", mlwh_sample.created AS "created", mlwh_events_event_types.key AS "event_type"
         FROM sample AS mlwh_sample
         JOIN stock_resource AS mlwh_stock_resource ON (mlwh_sample.id_sample_tmp = mlwh_stock_resource.id_sample_tmp)
         JOIN #{event_wh_db}.subjects mlwh_events_subjects ON (mlwh_events_subjects.friendly_name = mlwh_sample.sanger_sample_id)
@@ -16,8 +17,8 @@ class AddCherrypickedSamplesView < ActiveRecord::Migration[6.0]
         JOIN #{event_wh_db}.events mlwh_events_events ON (mlwh_events_roles.event_id = mlwh_events_events.id)
         JOIN #{event_wh_db}.event_types mlwh_events_event_types ON (mlwh_events_events.event_type_id = mlwh_events_event_types.id)
         UNION
-        SELECT mlwh_sample.description, mlwh_lh_sample.plate_barcode, mlwh_sample.phenotype,
-        mlwh_lh_sample.coordinate, mlwh_sample.created, mlwh_events_event_types.key AS "event_type"
+        SELECT mlwh_sample.description AS "root_sample_id", mlwh_lh_sample.plate_barcode AS "plate_barcode", mlwh_sample.phenotype AS "phenotype",
+        mlwh_lh_sample.coordinate AS "coordinate", mlwh_sample.created AS "created", mlwh_events_event_types.key AS "event_type"
         FROM sample as mlwh_sample
         JOIN lighthouse_sample AS mlwh_lh_sample ON (mlwh_sample.uuid_sample_lims = mlwh_lh_sample.lh_sample_uuid)
         JOIN #{event_wh_db}.subjects AS mlwh_events_subjects ON (mlwh_events_subjects.uuid = UNHEX(REPLACE(mlwh_lh_sample.lh_sample_uuid, '-', '')))
@@ -25,7 +26,7 @@ class AddCherrypickedSamplesView < ActiveRecord::Migration[6.0]
         JOIN #{event_wh_db}.events AS mlwh_events_events ON (mlwh_events_events.id = mlwh_events_roles.event_id)
         JOIN #{event_wh_db}.event_types AS mlwh_events_event_types ON (mlwh_events_event_types.id = mlwh_events_events.event_type_id)
       ) AS cp_sample
-      GROUP BY cp_sample.description, cp_sample.plate_barcode, cp_sample.phenotype, cp_sample.coordinate, cp_sample.created, cp_sample.event_type
+      GROUP BY cp_sample.root_sample_id, cp_sample.plate_barcode, cp_sample.phenotype, cp_sample.coordinate, cp_sample.created, cp_sample.event_type
     SQL
   end
 
