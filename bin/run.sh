798a493228e2375031f4f1dc70c84695584d76e3
Eduardo Martin Rojo <emr@sanger.ac.uk>
2021-02-05 14:11:32 +0000

    Added startup script

--- bin/run.sh
+++ bin/run.sh
@@ -1,8 +1,12 @@
-#!/bin/sh
-if test -z "${INTEGRATION_TEST_SETUP}" ; then
-  ./bin/amqp_client start
-else
+#!/bin/bash
+set -Eeuxo pipefail
+
+if test "${INTEGRATION_TEST_SETUP:-}" = "true" ; then
   echo "Setting up for integration tests"
   RAILS_ENV=test bundle exec rake db:reset
-  RAILS_ENV=test bundle exec rake db:views:schema:load
 fi
+
+echo "Starting the service"
+mkdir tmp/pids || true
+
+./bin/amqp_client start
