617ce9880cfb9b5af4698b2a0eb675f9f79e6e9a
Eduardo Martin Rojo <emr@sanger.ac.uk>
2021-02-11 11:32:46 +0000

    Change encoding

--- db/migrate/20210210111138_change_lighthouse_samples_table_charset.rb
+++ db/migrate/20210210111138_change_lighthouse_samples_table_charset.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+#
+# Change charset of lighthouse_sample to use utf8 instead of latin1
+#
 class ChangeLighthouseSamplesTableCharset < ActiveRecord::Migration[6.0]
   def up
     ActiveRecord::Base.connection.execute("ALTER TABLE `lighthouse_sample` CONVERT TO CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci';")
