951481a8414bc7f6d26160ececc2dd2be73e2fb9
Eduardo Martin Rojo <emr@sanger.ac.uk>
2021-02-11 11:39:01 +0000

    Changes in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_15_115653) do
+ActiveRecord::Schema.define(version: 2021_02_10_231042) do
 
   create_table "bmap_flowcell", primary_key: "id_bmap_flowcell_tmp", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
     t.datetime "last_updated", null: false, comment: "Timestamp of last update"
