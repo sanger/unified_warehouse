a9116d1f4b80e387efc89d07d0a5a07859e6afa4
Katy Taylor <kt17@sanger.ac.uk>
2021-04-08 11:59:05 +0100

    change migration to use change_table with bulk:true, for rubocop - had to add 'up' and 'down' methods, because otherwise it wouldn't roll back

--- db/migrate/20210408094808_add_sample_fields.rb
+++ db/migrate/20210408094808_add_sample_fields.rb
@@ -2,33 +2,68 @@
 
 # Add most of the fields from Sequencescape sample_metadata table that aren't already here
 # To avoid having to fulfil individual requests for fields to be copied over
+# rubocop:disable Metrics/BlockLength
 class AddSampleFields < ActiveRecord::Migration[6.0]
-  def change
-    add_column :sample, :sibling, :string
-    add_column :sample, :is_resubmitted, :boolean
-    add_column :sample, :date_of_sample_collection, :string
-    add_column :sample, :date_of_sample_extraction, :string
-    add_column :sample, :extraction_method, :string
-    add_column :sample, :purified, :string
-    add_column :sample, :purification_method, :string
-    add_column :sample, :customer_measured_concentration, :string
-    add_column :sample, :concentration_determined_by, :string
-    add_column :sample, :sample_type, :string
-    add_column :sample, :storage_conditions, :string
-    add_column :sample, :genotype, :string
-    add_column :sample, :age, :string
-    add_column :sample, :cell_type, :string
-    add_column :sample, :disease_state, :string
-    add_column :sample, :compound, :string
-    add_column :sample, :dose, :string
-    add_column :sample, :immunoprecipitate, :string
-    add_column :sample, :growth_condition, :string
-    add_column :sample, :organism_part, :string
-    add_column :sample, :time_point, :string
-    add_column :sample, :disease, :string
-    add_column :sample, :subject, :string
-    add_column :sample, :treatment, :string
-    add_column :sample, :date_of_consent_withdrawn, :datetime
-    add_column :sample, :marked_as_consent_withdrawn_by, :string
+  def up
+    change_table :sample, bulk: true do |t|
+      t.string :sibling, null: true
+      t.boolean :is_resubmitted, null: true
+      t.string :date_of_sample_collection, null: true
+      t.string :date_of_sample_extraction, null: true
+      t.string :extraction_method, null: true
+      t.string :purified, null: true
+      t.string :purification_method, null: true
+      t.string :customer_measured_concentration, null: true
+      t.string :concentration_determined_by, null: true
+      t.string :sample_type, null: true
+      t.string :storage_conditions, null: true
+      t.string :genotype, null: true
+      t.string :age, null: true
+      t.string :cell_type, null: true
+      t.string :disease_state, null: true
+      t.string :compound, null: true
+      t.string :dose, null: true
+      t.string :immunoprecipitate, null: true
+      t.string :growth_condition, null: true
+      t.string :organism_part, null: true
+      t.string :time_point, null: true
+      t.string :disease, null: true
+      t.string :subject, null: true
+      t.string :treatment, null: true
+      t.datetime :date_of_consent_withdrawn, null: true
+      t.string :marked_as_consent_withdrawn_by, null: true
+    end
+  end
+
+  def down
+    change_table :sample, bulk: true do |t|
+      t.remove :sibling
+      t.remove :is_resubmitted
+      t.remove :date_of_sample_collection
+      t.remove :date_of_sample_extraction
+      t.remove :extraction_method
+      t.remove :purified
+      t.remove :purification_method
+      t.remove :customer_measured_concentration
+      t.remove :concentration_determined_by
+      t.remove :sample_type
+      t.remove :storage_conditions
+      t.remove :genotype
+      t.remove :age
+      t.remove :cell_type
+      t.remove :disease_state
+      t.remove :compound
+      t.remove :dose
+      t.remove :immunoprecipitate
+      t.remove :growth_condition
+      t.remove :organism_part
+      t.remove :time_point
+      t.remove :disease
+      t.remove :subject
+      t.remove :treatment
+      t.remove :date_of_consent_withdrawn
+      t.remove :marked_as_consent_withdrawn_by
+    end
   end
 end
+# rubocop:enable Metrics/BlockLength
