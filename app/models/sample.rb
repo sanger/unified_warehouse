d5f6ccf7411e515e89fd8e585097194e8ce733a9
Katy Taylor <kt17@sanger.ac.uk>
2021-04-08 11:11:47 +0100

    Remove some fields from the 'translate' block in sample - These will be translated Sequencescape side from now on

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -17,14 +17,7 @@ class Sample < ApplicationRecord
 
     translate(
       id: :id_sample_lims,
-      uuid: :uuid_sample_lims,
-      sample_common_name: :common_name,
-      sample_description: :description,
-      sample_ebi_accession_number: :accession_number,
-      sample_taxon_id: :taxon_id,
-      sample_public_name: :public_name,
-      sample_sra_hold: :sample_visibility,
-      sample_strain_att: :strain
+      uuid: :uuid_sample_lims
     )
   end
 end
