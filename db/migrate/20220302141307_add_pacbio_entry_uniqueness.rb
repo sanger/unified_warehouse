d948c89d48454a9b087ef38600d4a366b2424f16
Eduardo Martin Rojo <emr@sanger.ac.uk>
2022-03-22 15:33:38 +0000

    Rubocop Column as string

--- db/migrate/20220302141307_add_pacbio_entry_uniqueness.rb
+++ db/migrate/20220302141307_add_pacbio_entry_uniqueness.rb
@@ -4,8 +4,8 @@
 class AddPacbioEntryUniqueness < ActiveRecord::Migration[6.0]
   def change
     change_table :pac_bio_run, bulk: true do |table|
-      table.virtual :comparable_tag_identifier, type: :integer, as: 'IFNULL(tag_identifier, -1)', description: 'It will contain the value of tag_identifier, or -1 if the value was null. This virtual column is intended to support SQL comparisons if needed.'
-      table.virtual :comparable_tag2_identifier, type: :integer, as: 'IFNULL(tag2_identifier, -1)', description: 'It will contain the value of tag2_identifier, or -1 if the value was null. This virtual column is intended to support SQL comparisons if needed.'
+      table.virtual :comparable_tag_identifier, type: :string, as: 'IFNULL(tag_identifier, -1)', description: 'It will contain the value of tag_identifier, or -1 if the value was null. This virtual column is intended to support SQL comparisons if needed.'
+      table.virtual :comparable_tag2_identifier, type: :string, as: 'IFNULL(tag2_identifier, -1)', description: 'It will contain the value of tag2_identifier, or -1 if the value was null. This virtual column is intended to support SQL comparisons if needed.'
     end
   end
 end
