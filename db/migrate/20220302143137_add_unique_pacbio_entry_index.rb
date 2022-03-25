60c4b310edd3f75ada0b44ef51ce650621eeed50
Eduardo Martin Rojo <emr@sanger.ac.uk>
2022-03-25 16:23:41 +0000

    Migration include lims

--- db/migrate/20220302143137_add_unique_pacbio_entry_index.rb
+++ db/migrate/20220302143137_add_unique_pacbio_entry_index.rb
@@ -4,7 +4,7 @@
 class AddUniquePacbioEntryIndex < ActiveRecord::Migration[6.0]
   def change
     add_index :pac_bio_run,
-              %i[id_pac_bio_run_lims well_label comparable_tag_identifier comparable_tag2_identifier],
+              %i[id_lims id_pac_bio_run_lims well_label comparable_tag_identifier comparable_tag2_identifier],
               unique: true, name: 'unique_pac_bio_entry'
   end
 end
