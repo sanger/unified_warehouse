8f5b4eccf1e91ead9d0f92bb1f3c4451ab25741c
Harriet Craven <hc6@sanger.ac.uk>
2022-04-19 11:51:13 +0100

    rubocopped

--- db/migrate/20141113130813_add_pool_id_column.rb
+++ db/migrate/20141113130813_add_pool_id_column.rb
@@ -6,8 +6,10 @@ class AddPoolIdColumn < ActiveRecord::Migration
   end
 
   def down
+    # rubocop:disable Rails/BulkChangeTable
     change_table :iseq_flowcell do |t|
       t.remove 'id_pool_lims', :string, limit: 20, null: false, comment: 'Most specific LIMs identifier associated with the pool'
     end
+    # rubocop:enable Rails/BulkChangeTable
   end
 end
