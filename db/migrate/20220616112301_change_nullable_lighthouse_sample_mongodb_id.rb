6155e5ae15a66f32d00354f756316466258f4e5f
Eduardo Martin Rojo <emr@sanger.ac.uk>
2022-06-16 12:47:06 +0100

    Added comment

--- db/migrate/20220616112301_change_nullable_lighthouse_sample_mongodb_id.rb
+++ db/migrate/20220616112301_change_nullable_lighthouse_sample_mongodb_id.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
+#
+# This migration disables NOT NULL to the mongodb_id column because the plan is
+# this table will be maintained by GSU team and they won't use that column anymore
 class ChangeNullableLighthouseSampleMongodbId < ActiveRecord::Migration[6.0]
   def change
     change_column_null(:lighthouse_sample, :mongodb_id, true)
