43bb23f6839f4ae5e56d3f92573aa09be938f7c1
Eduardo Martin Rojo <emr@sanger.ac.uk>
2022-06-16 14:28:34 +0100

    Bump migration version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_22_101440) do
+ActiveRecord::Schema.define(version: 2022_06_16_112301) do
 
   create_table "bmap_flowcell", primary_key: "id_bmap_flowcell_tmp", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
     t.datetime "last_updated", null: false, comment: "Timestamp of last update"
