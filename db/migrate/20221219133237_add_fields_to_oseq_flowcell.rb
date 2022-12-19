3612b8b1d1ae2e789b15fc31ccd7c8831e989d32
Stephen <519327+stevieing@users.noreply.github.com>
2022-12-19 13:35:59 +0000

    Added migration for oseq_flowcell

---
+++ db/migrate/20221219133237_add_fields_to_oseq_flowcell.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# These fields need to be added to support multiplexing
+# TODO: confirm with NPG which fields they need
+class AddFieldsToOseqFlowcell < ActiveRecord::Migration[6.0]
+  def change; end
+end
