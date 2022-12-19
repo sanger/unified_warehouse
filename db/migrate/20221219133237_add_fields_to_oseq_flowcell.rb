b150fb7037a60471c3fd1eab48564ddd0fa78070
Stephen <519327+stevieing@users.noreply.github.com>
2022-12-19 13:47:12 +0000

    Added flowcell id to migration.

--- db/migrate/20221219133237_add_fields_to_oseq_flowcell.rb
+++ db/migrate/20221219133237_add_fields_to_oseq_flowcell.rb
@@ -3,5 +3,9 @@
 # These fields need to be added to support multiplexing
 # TODO: confirm with NPG which fields they need
 class AddFieldsToOseqFlowcell < ActiveRecord::Migration[6.0]
-  def change; end
+  def change
+    change_table :long_read_qc_result, bulk: true do |t|
+      t.string :flowcell_id, comment: 'The id of the flowcell. Supplied with the flowcell. Format FAVnnnn'
+    end
+  end
 end
