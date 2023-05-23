6e49da43d04209da6cbc2da7408b56b938b40a2f
Ben Topping <ben.topping@outlook.com>
2023-05-23 12:01:18 +0100

    Linted and added comments

--- db/migrate/20230522131908_add_library_tube_barcode_to_pacbio_run.rb
+++ db/migrate/20230522131908_add_library_tube_barcode_to_pacbio_run.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Adds library tube barcode column to pac_bio_run and removes the validation for plate_barcodes precense
 class AddLibraryTubeBarcodeToPacbioRun < ActiveRecord::Migration[6.1]
   def up
     change_table :pac_bio_run, bulk: true do |t|
@@ -10,10 +13,10 @@ class AddLibraryTubeBarcodeToPacbioRun < ActiveRecord::Migration[6.1]
     change_table :pac_bio_run, bulk: true do |t|
       t.remove :pac_bio_library_tube_barcode
     end
-    # We need to add a placeholder value in case any plate barcodes were added as null since the migration
-    PacBioRun.where(plate_barcode: nil).each do |run| 
-      run.plate_barcode = "plate_barcode placeholder"
-      run.save 
+    # Here we add a placeholder value in case any plate barcodes were added as null since the migration
+    PacBioRun.where(plate_barcode: nil).each do |run|
+      run.plate_barcode = 'plate_barcode placeholder'
+      run.save
     end
     change_column :pac_bio_run, :plate_barcode, :string, null: false
   end
