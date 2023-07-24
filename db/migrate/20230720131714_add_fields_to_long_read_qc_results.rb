e58a413423a87036aa2f40a0f4398c75fa91adf7
Sangeetha <sb67@sanger.ac.uk>
2023-07-24 12:55:21 +0100

    Fixed rubocop errors

--- db/migrate/20230720131714_add_fields_to_long_read_qc_results.rb
+++ db/migrate/20230720131714_add_fields_to_long_read_qc_results.rb
@@ -1,7 +1,12 @@
+# frozen_string_literal: true
+
+# Adds new optional fields to long_read_qc_result
 class AddFieldsToLongReadQcResults < ActiveRecord::Migration[7.0]
   def change
-    add_column :long_read_qc_result, :priority_level, :string, comment: 'Priority level eg Medium, High etc'
-    add_column :long_read_qc_result, :date_required_by, :string, comment: 'Date required by eg tol, etc'
-    add_column :long_read_qc_result, :reason_for_priority, :string, comment: 'Reason for priority'
+    change_table :long_read_qc_result, bulk: true do |t|
+      t.string :priority_level, comment: 'Priority level eg Medium, High etc'
+      t.string :date_required_by, comment: 'Date required by eg tol, etc'
+      t.string :reason_for_priority, comment: 'Reason for priority'
+    end
   end
 end
