7a5b9e0e1b82db289da2dc7b05780e1fe5dab5eb
Eduardo Martin Rojo <emr@sanger.ac.uk>
2023-08-16 10:45:41 +0100

    Added schema

--- db/migrate/20230816092204_add_index_long_read_qc_results_sample_id.rb
+++ db/migrate/20230816092204_add_index_long_read_qc_results_sample_id.rb
@@ -3,6 +3,6 @@
 # Add index on sample id for long read qc results to be able to join with samples
 class AddIndexLongReadQcResultsSampleId < ActiveRecord::Migration[7.0]
   def change
-    add_index :long_read_qc_results, :sample_id, unique: false
+    add_index :long_read_qc_result, :sample_id, unique: false
   end
 end
