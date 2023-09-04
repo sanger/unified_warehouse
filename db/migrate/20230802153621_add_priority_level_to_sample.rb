4fa8371c7aec51b51db704897f0f9395ed5fe0fd
Eduardo Martín Rojo <emrojo@users.noreply.github.com>
2023-09-04 15:36:10 +0100

    Apply suggestions from code review
    Co-authored-by: Harriet Craven <harrietc52@users.noreply.github.com>

--- db/migrate/20230802153621_add_priority_level_to_sample.rb
+++ db/migrate/20230802153621_add_priority_level_to_sample.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-# Adding priority_level eg Medium, High etc to recieve this information
-# in a sample message from TOL through traction receptions endpoint
+# Adding priority_level e.g. Medium, High etc to receive this information
+# in a sample message from TOL through traction qc_receptions endpoint
 class AddPriorityLevelToSample < ActiveRecord::Migration[7.0]
   def change
     change_table :sample, bulk: true do |t|
-      t.string :priority_level, comment: 'Priority level eg Medium, High etc'
+      t.string :priority_level, comment: 'Priority level e.g. Medium, High etc'
     end
   end
 end
