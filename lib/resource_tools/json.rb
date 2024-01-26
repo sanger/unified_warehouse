bfd769891f9f96d79aad9e5e0d10463265981c17
Stephen <519327+stevieing@users.noreply.github.com>
2024-01-26 15:41:54 +0000

    Disable block forwarding in JSON module

--- lib/resource_tools/json.rb
+++ lib/resource_tools/json.rb
@@ -146,7 +146,7 @@ module ResourceTools::Json
       end
     end
 
-    # rubocop:disable Style/ArgumentsForwarding
+    # rubocop:disable Naming/BlockForwarding
     def initialize(*args, &block)
       super
       if self.class.custom_values.present?
@@ -157,7 +157,7 @@ module ResourceTools::Json
       convert_booleans
       delete_if { |k, _| ignoreable.include?(k) }
     end
-    # rubocop:enable Style/ArgumentsForwarding
+    # rubocop:enable Naming/BlockForwarding
 
     def convert_booleans
       self.stored_as_boolean.each do |key|
