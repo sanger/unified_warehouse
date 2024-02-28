9a14706f754a0fdad79d8119ed8f3841dadd6208
Dasun Pubudumal <pubudumald@gmail.com>
2024-02-28 14:19:15 +0000

    Updating the migration to up-down execution (up for updating, down for rolling back)

--- db/migrate/20240228140011_update_iseq_flowcell_entity_type_comment.rb
+++ db/migrate/20240228140011_update_iseq_flowcell_entity_type_comment.rb
@@ -1,5 +1,9 @@
 class UpdateIseqFlowcellEntityTypeComment < ActiveRecord::Migration[7.0]
-  def change
-    change_column_comment :iseq_flowcell, :entity_type, "Lane type: library, library_control, library_indexed, library_indexed_spike."
+  def up
+    change_column_comment :iseq_flowcell, :entity_type, "Lane type: library, library_control, library_indexed, library_indexed_spike"
+  end
+
+  def down
+    change_column_comment :iseq_flowcell, :entity_type, "Lane type: library, pool, library_control, library_indexed, library_indexed_spike"
   end
 end
