e1d9f81a2909d8b82613925195a385b52797108a
yoldas <ay6@sanger.ac.uk>
2024-03-12 21:16:31 +0000

    Rubocop

--- lib/resource_tools/json.rb
+++ lib/resource_tools/json.rb
@@ -146,7 +146,6 @@ module ResourceTools::Json
       end
     end
 
-    # rubocop:disable Style/ArgumentsForwarding
     def initialize(*args, &block)
       super
       if self.class.custom_values.present?
@@ -157,7 +156,6 @@ module ResourceTools::Json
       convert_booleans
       delete_if { |k, _| ignoreable.include?(k) }
     end
-    # rubocop:enable Style/ArgumentsForwarding
 
     def convert_booleans
       self.stored_as_boolean.each do |key|
