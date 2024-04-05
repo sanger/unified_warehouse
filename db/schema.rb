e11e790b0899dc376cacacacfc88ae7d180d8590
Dasun Pubudumal <pubudumald@gmail.com>
2024-04-05 11:36:25 +0100

    Adding collation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2024_04_04_142248) do
-  create_table "aliquot", charset: "utf8mb3", id: :integer, force: :cascade do |t|
+  create_table "aliquot", charset: "utf8mb3", id: :integer,  collation: "utf8mb3_unicode_ci", force: :cascade do |t|
     t.string "lims_source", null: false, comment: "The LIMS system that the aliquot was created in"
     t.string "lims_uuid", null: false, comment: "UUID of the LIMS system"
     t.string "aliquot_type", null: false, comment: "The type of the aliquot"
