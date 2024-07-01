a126cb7a1ad049ce3029e10fb303e7ee62fc0ce0
Stephen <519327+stevieing@users.noreply.github.com>
2024-07-01 10:55:24 +0100

    Modified aliquots factory.

--- spec/factories.rb
+++ spec/factories.rb
@@ -96,5 +96,7 @@ FactoryBot.define do
     volume { 24.98589115 }
     concentration { 12.46979445 }
     last_updated { Time.new(2012, 3, 11, 10, 20, 8, '+00:00') }
+    created_at { Time.new(2012, 3, 11, 10, 20, 8, '+00:00') }
+    recorded_at { Time.new(2012, 3, 11, 10, 20, 8, '+00:00') }
   end
 end
