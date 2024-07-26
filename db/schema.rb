d41dea30e0e78979cf4cbf1cefb6108dc620e040
sabrine33 <sa33@sanger.ac.uk>
2024-07-26 15:37:17 +0100

    resolve conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_08_081135) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_24_144156) do
   create_table "aliquot", charset: "utf8mb3", collation: "utf8mb3_unicode_ci", force: :cascade do |t|
     t.string "id_lims", null: false, comment: "The LIMS system that the aliquot was created in"
     t.string "aliquot_uuid", null: false, comment: "The UUID of the aliquot in the LIMS system"
