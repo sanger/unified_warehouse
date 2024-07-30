bbaaf5d75fe61c912c31dc359c54cb315491f5e8
Dasun Pubudumal <dp27@sanger.ac.uk>
2024-07-30 08:58:24 +0100

    Adding retention instructions

--- db/migrate/20240730074446_add_retention_instruction_column_to_sample_table.rb
+++ db/migrate/20240730074446_add_retention_instruction_column_to_sample_table.rb
@@ -1,9 +1,5 @@
 class AddRetentionInstructionColumnToSampleTable < ActiveRecord::Migration[7.0]
-  def up
+  def change
     add_column :sample, :retention_instruction, :string
-  end
-
-  def down
-    remove_column :sample, :retention_instruction
   end
 end
