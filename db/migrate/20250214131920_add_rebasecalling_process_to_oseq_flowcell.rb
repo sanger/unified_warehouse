94f8cd5119be781d980342a5d512425ec264ce13
Stephen <519327+stevieing@users.noreply.github.com>
2025-02-17 12:59:34 +0000

    Add comment to rebasecalling_process column in oseq_flowcell table

--- db/migrate/20250214131920_add_rebasecalling_process_to_oseq_flowcell.rb
+++ db/migrate/20250214131920_add_rebasecalling_process_to_oseq_flowcell.rb
@@ -1,5 +1,5 @@
 class AddRebasecallingProcessToOseqFlowcell < ActiveRecord::Migration[7.0]
   def change
-    add_column :oseq_flowcell, :rebasecalling_process, :string, limit: 50
+    add_column :oseq_flowcell, :rebasecalling_process, :string, limit: 50, comment: 'Settings required for modified basecalling'
   end
 end
