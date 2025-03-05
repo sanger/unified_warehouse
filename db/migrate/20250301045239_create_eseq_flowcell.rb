1eabe65aa2364c01dee8a2720797ef8672a2a980
yoldas <ay6@sanger.ac.uk>
2025-03-05 22:45:48 +0000

    Add id_flowcell_lims, indexes, and foreign key constraints

--- db/migrate/20250301045239_create_eseq_flowcell.rb
+++ db/migrate/20250301045239_create_eseq_flowcell.rb
@@ -8,7 +8,8 @@ class CreateEseqFlowcell < ActiveRecord::Migration[7.0]
       t.datetime :recorded_at, null: false, comment: 'Timestamp of warehouse update'
       t.column :id_sample_tmp, 'integer unsigned', null: false, comment: 'Sample id, see "sample.id_sample_tmp"'
       t.column :id_study_tmp, 'integer unsigned', null: true, comment: 'Study id, see "study.id_study_tmp"'
-      t.string :id_lims, limit: 10, null: false, comment: 'LIM system identifier, e.g. CLARITY-GCLP, SEQSCAPE'
+      t.string :id_lims, limit: 10, null: false, comment: 'LIM system identifier, e.g. CLARITY-GCLP, SEQSCAPE', index: true
+      t.string :id_flowcell_lims, limit:20, null: false, comment: 'LIMs-specific flowcell id, batch_id for Sequencescape', index: true
       t.column :lane, 'smallint unsigned', null: false, comment: 'Flowcell lane number, 1 or 2'
       t.string :entity_type, limit: 30, null: false, comment: 'Library type: library_indexed, library_indexed_spike'
       t.string :tag_sequence, limit: 30, null: true, comment: 'Tag sequence'
@@ -17,9 +18,13 @@ class CreateEseqFlowcell < ActiveRecord::Migration[7.0]
       t.string :bait_name, limit: 50, null: true, comment: 'WTSI-wide name that uniquely identifies a bait set'
       t.column :requested_insert_size_from, 'integer unsigned', null: true, comment: 'Requested insert size min value'
       t.column :requested_insert_size_to, 'integer unsigned', null: true, comment: 'Requested insert size max value'
-      t.string :id_pool_lims, limit: 20, null: false, comment: 'Most specific LIMs identifier associated with the pool'
-      t.string :id_library_lims, limit: 255, null: true, comment: 'Earliest LIMs identifier associated with library creation'
+      t.string :id_pool_lims, limit: 20, null: false, comment: 'Most specific LIMs identifier associated with the pool', index: true
+      t.string :id_library_lims, limit: 255, null: true, comment: 'Earliest LIMs identifier associated with library creation', index: true
       t.string :primer_panel, limit: 255, null: true, comment: 'Primer Panel name'
+
     end
+
+    add_foreign_key :eseq_flowcell, :samples, column: :id_sample_tmp, primary_key: :id_sample_tmp
+    add_foreign_key :eseq_flowcell, :studies, column: :id_study_tmp, primary_key: :id_study_tmp
   end
 end
