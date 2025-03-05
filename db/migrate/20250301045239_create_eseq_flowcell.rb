3910ac08b182d0aeea8aaed43d59502fc38d3e7a
yoldas <ay6@sanger.ac.uk>
2025-03-05 22:52:05 +0000

    Remove duplicate id_flowcell_lims definition

--- db/migrate/20250301045239_create_eseq_flowcell.rb
+++ db/migrate/20250301045239_create_eseq_flowcell.rb
@@ -2,14 +2,13 @@ class CreateEseqFlowcell < ActiveRecord::Migration[7.0]
   def change
     create_table :eseq_flowcell, id: false, options: 'CHARSET=utf8 COLLATE=utf8_unicode_ci' do |t|
       t.column :id_eseq_flowcell_tmp, 'integer unsigned auto_increment', primary_key: true, comment: 'Internal to this database, id value can change'
-      t.string :id_flowcell_lims, limit: 255, null: false, comment: 'LIMs-specific flowcell id'
+      t.string :id_flowcell_lims, limit: 255, null: false, comment: 'LIMs-specific flowcell id', index: true
       t.string :run_name, limit: 80, null: false, comment: 'Run name as given to the instrument'
       t.datetime :last_updated, null: false, comment: 'Timestamp of last update'
       t.datetime :recorded_at, null: false, comment: 'Timestamp of warehouse update'
       t.column :id_sample_tmp, 'integer unsigned', null: false, comment: 'Sample id, see "sample.id_sample_tmp"'
       t.column :id_study_tmp, 'integer unsigned', null: true, comment: 'Study id, see "study.id_study_tmp"'
       t.string :id_lims, limit: 10, null: false, comment: 'LIM system identifier, e.g. CLARITY-GCLP, SEQSCAPE', index: true
-      t.string :id_flowcell_lims, limit:20, null: false, comment: 'LIMs-specific flowcell id, batch_id for Sequencescape', index: true
       t.column :lane, 'smallint unsigned', null: false, comment: 'Flowcell lane number, 1 or 2'
       t.string :entity_type, limit: 30, null: false, comment: 'Library type: library_indexed, library_indexed_spike'
       t.string :tag_sequence, limit: 30, null: true, comment: 'Tag sequence'
