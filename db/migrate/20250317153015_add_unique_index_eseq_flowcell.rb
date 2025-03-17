c81abc3cdaee72b5be2a8a68d1cac5f29253be0e
yoldas <ay6@sanger.ac.uk>
2025-03-17 15:32:21 +0000

    Add unique index on multiple columns of eseq flowcell

---
+++ db/migrate/20250317153015_add_unique_index_eseq_flowcell.rb
@@ -0,0 +1,5 @@
+class AddUniqueIndexEseqFlowcell < ActiveRecord::Migration[7.1]
+  def change
+    add_index :eseq_flowcell, %i[id_flowcell_lims lane tag_index id_lims], unique: true, name: 'index_eseq_flowcell_id_flowcell_lims_position_tag_index_id_lims'
+  end
+end
