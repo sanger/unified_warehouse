ebbbe2cd4380284fec3ce68638207ca5be7834eb
yoldas <ay6@sanger.ac.uk>
2025-03-24 17:50:03 +0000

    Rename migration class

--- db/migrate/20250317141219_add_eseq_flowcell_composition_key_columns.rb
+++ db/migrate/20250317141219_add_eseq_flowcell_composition_key_columns.rb
@@ -2,7 +2,7 @@
 
 # This migration adds entity_id_lims and tag_index to the existing eseq_flowcell
 # table to make the record handling similar to the iseq_flowcell table.
-class AddEseqFlowcellTagColumns < ActiveRecord::Migration[7.1]
+class AddEseqFlowcellCompositionKeyColumns < ActiveRecord::Migration[7.1]
   def change
     # sequencecape aliquot_index is mapped to tag_index
     add_column :eseq_flowcell, :tag_index, 'smallint unsigned', null: true,
