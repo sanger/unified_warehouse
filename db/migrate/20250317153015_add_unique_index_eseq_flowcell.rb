8bbb14eccdb8438ebbf077f19b842870edce6349
yoldas <ay6@sanger.ac.uk>
2025-03-24 17:41:54 +0000

    Add model composition keys to unique index

--- db/migrate/20250317153015_add_unique_index_eseq_flowcell.rb
+++ db/migrate/20250317153015_add_unique_index_eseq_flowcell.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
-# This migration adds a multicolumn unique index to the eseq_flowcell table.
+# Adds a multicolumn unique index to the eseq_flowcell table.
 class AddUniqueIndexEseqFlowcell < ActiveRecord::Migration[7.1]
   def change
-    add_index :eseq_flowcell, %i[id_flowcell_lims lane tag_index id_lims], unique: true, name: 'index_eseq_flowcell_id_flowcell_lims_position_tag_index_id_lims'
+    add_index :eseq_flowcell, %i[
+      tag_index
+      id_flowcell_lims
+      entity_id_lims
+      entity_type
+      lane
+      tag_sequence
+      tag2_sequence
+    ], unique: true, name: 'index_eseq_flowcell_composition_keys'
   end
 end
