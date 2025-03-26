208b101fb6f710d85aaaec686262606eac3de885
yoldas <ay6@sanger.ac.uk>
2025-03-26 11:54:20 +0000

    Add rollback and index naming to remove index on id_lims

--- db/migrate/20250324172139_remove_index_eseq_flowcell_id_lims.rb
+++ db/migrate/20250324172139_remove_index_eseq_flowcell_id_lims.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
+# This migration removes the index on the id_lims column from the initial
+# eseq_flowcell table because it is coarse and inefficient.
 class RemoveIndexEseqFlowcellIdLims < ActiveRecord::Migration[7.1]
+  INDEX_NAME = 'index_eseq_flowcell_on_id_lims'
   def up
-    remove_index :eseq_flowcell, name: 'id_lims', if_exists: true
+    remove_index :eseq_flowcell, name: INDEX_NAME, if_exists: true
   end
 
   def down
-    add_index :eseq_flowcell, :id_lims, name: 'id_lims'
+    add_index :eseq_flowcell, :id_lims, name: INDEX_NAME
   end
 end
