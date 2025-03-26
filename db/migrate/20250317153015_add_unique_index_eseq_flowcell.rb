2af5649690dd3c0ae13c5a3ac12b0d6ac29ac264
yoldas <ay6@sanger.ac.uk>
2025-03-26 11:51:57 +0000

    Add rollback and unique index naming

--- db/migrate/20250317153015_add_unique_index_eseq_flowcell.rb
+++ db/migrate/20250317153015_add_unique_index_eseq_flowcell.rb
@@ -2,12 +2,20 @@
 
 # Adds a multicolumn unique index to the eseq_flowcell table.
 class AddUniqueIndexEseqFlowcell < ActiveRecord::Migration[7.1]
-  def change
+  # Max length of the index name is 64 characters.
+  # The composition keys are used to identify the rows in the nested sections
+  # of the flowcell JSON data. The index is unique on these keys.
+  INDEX_NAME = 'index_eseq_flowcell_on_composition_keys'
+  def up
     add_index :eseq_flowcell, %i[
       id_flowcell_lims
       lane
       tag_sequence
       tag2_sequence
-    ], unique: true, name: 'index_eseq_flowcell_composition_keys'
+      id_lims
+    ], unique: true, name: INDEX_NAME
+  end
+  def down
+    remove_index :eseq_flowcell, name: INDEX_NAME
   end
 end
