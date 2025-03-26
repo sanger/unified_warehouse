21c3fa5cc5db12667fe951673f9a1ec05096a726
yoldas <ay6@sanger.ac.uk>
2025-03-26 11:50:17 +0000

    Add comment to the initial migration

--- db/migrate/20250301045239_create_eseq_flowcell.rb
+++ db/migrate/20250301045239_create_eseq_flowcell.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# This migration creates the initial eseq_flowcell table.
 class CreateEseqFlowcell < ActiveRecord::Migration[7.0]
   def change
     create_table :eseq_flowcell, id: false, options: 'CHARSET=utf8 COLLATE=utf8_unicode_ci' do |t|
