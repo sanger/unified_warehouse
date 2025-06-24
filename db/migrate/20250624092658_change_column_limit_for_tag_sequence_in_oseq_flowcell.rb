1f11a718ebaeb6d290ee5dcaa24ec64457c3679d
Shiv <44001656+SHIV5T3R@users.noreply.github.com>
2025-06-24 10:50:31 +0100

    Removed changes to tag_sequence limit in iseq/oseq tables

--- db/migrate/20250624092658_change_column_limit_for_tag_sequence_in_oseq_flowcell.rb
+++
@@ -1,6 +0,0 @@
-class ChangeColumnLimitForTagSequenceInOseqFlowcell < ActiveRecord::Migration[7.2]
-  def change
-    change_column :oseq_flowcell, :tag_sequence, :string, limit: 50
-    change_column :oseq_flowcell, :tag2_sequence, :string, limit: 50
-  end
-end
