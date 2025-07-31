54ff34090d19d430ac8fee605a6bc4d91fb69697
Dasun Pubudumal <pubudumald@gmail.com>
2025-07-31 15:29:24 +0100

    Adding comment_type

--- db/migrate/20250730122510_add_comments_table.rb
+++ db/migrate/20250730122510_add_comments_table.rb
@@ -1,7 +1,7 @@
 class AddCommentsTable < ActiveRecord::Migration[7.2]
   def change
     create_table :comments do |t|
-      t.text :content
+      t.text :value
       t.string :batch_id, null: false
       t.string :id_lims, limit: 10, null: false
       t.integer :position, null: false, limit: 2, unsigned: true
