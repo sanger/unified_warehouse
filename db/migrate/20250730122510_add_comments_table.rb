2504b30ea51989b52dde169eb6b340a2138c41a3
Dasun Pubudumal <pubudumald@gmail.com>
2025-07-31 11:27:41 +0100

    Change schema for comments

--- db/migrate/20250730122510_add_comments_table.rb
+++ db/migrate/20250730122510_add_comments_table.rb
@@ -4,17 +4,9 @@ class AddCommentsTable < ActiveRecord::Migration[7.2]
       t.text :content
       t.string :batch_id, null: false
       t.string :id_lims, limit: 10, null: false
+      t.integer :position, null: false, limit: 2, unsigned: true
+      t.integer :tag_index, null: false, limit: 2, unsigned: true
       t.timestamps
     end
-    add_column :comments, :position, :integer, limit: 2, null: false
-    execute <<-SQL
-      ALTER TABLE comments
-      MODIFY COLUMN position SMALLINT UNSIGNED;
-    SQL
-    add_column :comments, :tag_index, :integer, limit: 2, null: false
-    execute <<-SQL
-      ALTER TABLE comments
-      MODIFY COLUMN tag_index SMALLINT UNSIGNED;
-    SQL
   end
 end
