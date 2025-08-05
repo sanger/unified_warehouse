fdb9e0e2a88e01113af2c0cab54cd88c62b4e82a
Dasun Pubudumal <pubudumald@gmail.com>
2025-08-05 13:45:03 +0100

    Update comments table migration to allow null values for batch_id, position, and tag_index

--- db/migrate/20250730122510_add_comments_table.rb
+++ db/migrate/20250730122510_add_comments_table.rb
@@ -2,10 +2,10 @@ class AddCommentsTable < ActiveRecord::Migration[7.2]
   def change
     create_table :comments do |t|
       t.text :comment_value, comment: 'Value of the comment corresponding to the comment_type'
-      t.string :batch_id, null: false, comment: 'Corresponds to id_flowcell_lims in iseq_flowcell table.'
+      t.string :batch_id, null: true, comment: 'Corresponds to id_flowcell_lims in iseq_flowcell table.'
       t.string :id_lims, limit: 10, null: false, comment: 'ID of the LIMS'
-      t.integer :position, null: false, limit: 2, unsigned: true, comment: 'Position of the lane in the flowcell'
-      t.integer :tag_index, null: false, limit: 2, unsigned: true, comment: 'Index of the tag (check iseq_flowcell tag_index column)'
+      t.integer :position, null: true, limit: 2, unsigned: true, comment: 'Position of the lane in the flowcell'
+      t.integer :tag_index, null: true, limit: 2, unsigned: true, comment: 'Index of the tag (check iseq_flowcell tag_index column)'
       t.string :comment_type, null: false, comment: 'Type of the comment e.g., under_representation'
 
       t.timestamps
