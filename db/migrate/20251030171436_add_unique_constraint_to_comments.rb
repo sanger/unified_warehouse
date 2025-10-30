7e213a84bb292f2d48a9e8f7c1a605c252917d5d
sabrine33 <sa33@sanger.ac.uk>
2025-10-30 17:34:07 +0000

    add table unique constraint

--- db/migrate/20251030171436_add_unique_constraint_to_comments.rb
+++ db/migrate/20251030171436_add_unique_constraint_to_comments.rb
@@ -1,7 +1,7 @@
 class AddUniqueConstraintToComments < ActiveRecord::Migration[7.2]
   def change
-    add_constraint :comments,
-                   :comments_unique_constraint,
-                   unique: [:batch_id, :id_lims, :position, :tag_index, :comment_type, :comment_value]
+    add_index :comments,
+              %i[batch_id id_lims position tag_index comment_type comment_value],
+              unique: true, name: 'comments_unique_constraint', length: { comment_value: 255 }  # specify prefix length for TEXT/BLOB
   end
 end
