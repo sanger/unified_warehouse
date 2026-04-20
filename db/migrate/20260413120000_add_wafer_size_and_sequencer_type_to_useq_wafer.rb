a6f837919c331f31ff51aad6af69bc46e4f9ee51
Wendy Yang <wy1@sanger.ac.uk>
2026-04-20 13:07:40 +0100

    update the column size

--- db/migrate/20260413120000_add_wafer_size_and_sequencer_type_to_useq_wafer.rb
+++ db/migrate/20260413120000_add_wafer_size_and_sequencer_type_to_useq_wafer.rb
@@ -2,7 +2,7 @@
 
 class AddWaferSizeAndSequencerTypeToUseqWafer < ActiveRecord::Migration[7.2]
   def change
-    add_column :useq_wafer, :wafer_size, :string, limit: 30, comment: "Wafer size"
-    add_column :useq_wafer, :sequencer_type, :string, limit: 30, comment: "Sequencer type"
+    add_column :useq_wafer, :wafer_size, :string, limit: 10, comment: "Wafer size, e.g. 10TB"
+    add_column :useq_wafer, :sequencer_type, :string, limit: 10, comment: "Sequencer type, e.g UG200"
   end
 end
