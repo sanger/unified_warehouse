fccd87c0eed93f5954b40a56ab440f91f3be5652
Wendy Yang <wy1@sanger.ac.uk>
2026-05-05 15:47:55 +0100

    update the comment

--- db/migrate/20260413120000_add_wafer_size_and_sequencer_type_to_useq_wafer.rb
+++ db/migrate/20260413120000_add_wafer_size_and_sequencer_type_to_useq_wafer.rb
@@ -3,6 +3,6 @@
 class AddWaferSizeAndSequencerTypeToUseqWafer < ActiveRecord::Migration[7.2]
   def change
     add_column :useq_wafer, :wafer_size, :string, limit: 10, comment: "Used to track UG200 wafer size only, defined in the sequencing request by SSR."
-    add_column :useq_wafer, :requested_sequencer_type, :string, limit: 10, comment: "Requested sequencer model type, 'UG 100' & 'UG 200'"
+    add_column :useq_wafer, :requested_sequencer_type, :string, limit: 10, comment: "Requested sequencer model, 'UG 100' & 'UG 200'"
   end
 end
