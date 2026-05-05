f4a9b8b5ce0811a9cacdffe31b1388d147f74460
Wendy Yang <wy1@sanger.ac.uk>
2026-05-05 15:11:51 +0100

    remove the new migration file

--- db/migrate/20260505131226_update_comments_on_useq_wafer_column.rb
+++
@@ -1,6 +0,0 @@
-class UpdateCommentsOnUseqWaferColumn < ActiveRecord::Migration[7.2]
-  def change
-    change_column_comment :useq_wafer, :wafer_size, "Used to track UG 200 wafer size only, defined in the sequencing request by SSR"
-    change_column_comment :useq_wafer, :requested_sequencer_type, "Requested sequencer model, 'UG 100' or 'UG 200'"
-  end
-end
