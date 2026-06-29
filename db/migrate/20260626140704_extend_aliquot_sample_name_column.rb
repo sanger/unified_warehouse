1fa367a4604bc8f17fa65ea50062a981eaa40001
Ben Topping <ben.topping@outlook.com>
2026-06-29 08:32:53 +0100

    feat(aliquot): adds more detail to sample_name comment

--- db/migrate/20260626140704_extend_aliquot_sample_name_column.rb
+++ db/migrate/20260626140704_extend_aliquot_sample_name_column.rb
@@ -1,5 +1,6 @@
 class ExtendAliquotSampleNameColumn < ActiveRecord::Migration[7.2]
-  def change
+  def up
     change_column :aliquot, :sample_name, :string, limit: 512
+    change_column_comment :aliquot, :sample_name, 'The name of the sample, or colon separated list of sample names, that the aliquot was created from'
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_06_26_140704) do
     t.string "aliquot_type", null: false, comment: "The type of the aliquot"
     t.string "source_type", null: false, comment: "The type of the source of the aliquot"
     t.string "source_barcode", null: false, comment: "The barcode of the source of the aliquot"
-    t.string "sample_name", limit: 512, null: false, comment: "The name of the sample that the aliquot was created from"
+    t.string "sample_name", limit: 512, null: false, comment: "The name of the sample, or colon separated list of sample names, that the aliquot was created from"
     t.string "used_by_type", null: false, comment: "The type of the entity that the aliquot is used by"
     t.string "used_by_barcode", null: false, comment: "The barcode of the entity that the aliquot is used by"
     t.decimal "volume", precision: 10, scale: 2, null: false, comment: "The volume of the aliquot (uL)"
