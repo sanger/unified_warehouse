2989e237368571907582b06993084b9fd6b74c3b
Wendy Yang <wy1@sanger.ac.uk>
2026-07-20 13:19:39 +0100

    add after run migration schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_07_01_125103) do
+ActiveRecord::Schema[7.2].define(version: 2026_07_20_105951) do
   create_table "aliquot", charset: "utf8mb3", collation: "utf8mb3_unicode_ci", force: :cascade do |t|
     t.string "id_lims", null: false, comment: "The LIMS system that the aliquot was created in"
     t.string "aliquot_uuid", null: false, comment: "The UUID of the aliquot in the LIMS system"
@@ -510,6 +510,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_07_01_125103) do
     t.string "ebi_library_selection"
     t.string "data_release_timing_publication_comment"
     t.string "data_share_in_preprint"
+    t.string "data_release_prevention_reason", comment: "Reason for preventing data release for this study"
     t.index ["accession_number"], name: "study_accession_number_index"
     t.index ["id_lims", "id_study_lims"], name: "study_id_lims_id_study_lims_index", unique: true
     t.index ["id_study_lims"], name: "index_study_on_id_study_lims"
@@ -560,7 +561,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_07_01_125103) do
     t.string "amp_assign_control_bead_tube", comment: "AMP assign control bead tube barcode"
     t.string "amp_instrument_name", comment: "AMP instrument name"
     t.string "wafer_size", limit: 10, comment: "Used to track UG200 wafer size only, defined in the sequencing request by SSR."
-    t.string "requested_sequencer_type", limit: 10, comment: "Requested sequencer model, 'UG 100' & 'UG 200'"
+    t.string "requested_sequencer_type", limit: 10, comment: "Requested sequencer model type, 'UG 100' & 'UG 200'"
     t.index ["batch_for_opentrons", "request_order", "tag_sequence", "id_lims"], name: "index_useq_wafer_on_composition_keys", unique: true
     t.index ["id_library_lims"], name: "index_useq_wafer_on_id_library_lims"
     t.index ["id_lims"], name: "index_useq_wafer_on_id_lims"
